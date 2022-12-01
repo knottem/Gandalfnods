e01138835ae2199eab3d2ecea5ecf8fd26521970
knottem <59744487+knottem@users.noreply.github.com>
2022-12-01 15:23:13 +0100

    Updated text
    cleaner text printing out

--- src/GandalfNods.java
+++ src/GandalfNods.java
@@ -1,9 +1,9 @@
 
-import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-public class GandalfNods {
 //  Gandalf nods his head 2.1 times a second-ish
 //  126 times a minute.
 //  7560 times a hour.
@@ -11,13 +11,18 @@ public class GandalfNods {
 //  181,440 in a day.
 //  63,072,000 after an entire year.
 
+public class GandalfNods {
+
     LocalDateTime start = LocalDateTime.of(2012,9,12,0,0,0);
+    LocalDate startDate;
+    DecimalFormat df = new DecimalFormat("###,###,###");
 
     void calculateTotalNods(){
         LocalDateTime currentTime = LocalDateTime.now();
+        startDate = LocalDate.from(start);
         long seconds = ChronoUnit.SECONDS.between(start,currentTime);
         long amount = (long) (seconds*2.1);
-        System.out.println("In the Video Gandalf Sax Guy, Gandalf has nod his head " + new BigDecimal(amount).toPlainString() + " times since " + start);
+        System.out.println("In the Video Gandalf Sax Guy, Gandalf has nod his head " +  df.format(amount) + " times since " + startDate);
     }
 
     public static void main(String[] args) {
